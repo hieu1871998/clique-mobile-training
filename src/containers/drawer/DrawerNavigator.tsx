c0dd420fad7d265dda84c96632f62c58ea05d6ad
Hieu Vi <hieu1871998@gmail.com>
2022-10-17 18:00:06 +0700

    Update drawer header colors
    Signed-off-by: Hieu Vi <hieu1871998@gmail.com>

--- src/containers/drawer/DrawerNavigator.tsx
+++ src/containers/drawer/DrawerNavigator.tsx
@@ -1,11 +1,22 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Colors } from 'constants'
 import { RootContainer, SettingsContainer } from 'containers'
 
 const Drawer = createDrawerNavigator()
 
 export const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.Primary,
+        },
+        headerTitleStyle: {
+          color: '#ffffff'
+        },
+        headerTintColor: '#ffffff'
+      }}
+    >
       <Drawer.Screen
         name='App'
         component={RootContainer}
